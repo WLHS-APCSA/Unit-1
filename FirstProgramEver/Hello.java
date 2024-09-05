7d085f4b997ad042e97e29dcbe2a9b169c06b9c8
Brandon Smith <88860661+smithbwlhs@users.noreply.github.com>
2024-09-05 10:36:43 -0700

    Add files via upload

---
+++ FirstProgramEver/Hello.java
@@ -0,0 +1,18 @@
+package FirstProgramEver;
+
+
+/**
+ * Write a description of class Hello here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Hello
+{
+   public static void main(String[] args){
+       System.out.println("Hello!"); //println prints Hello then goes to next line
+       System.out.print("My name is "); //print prints arguments then cursor stays on line
+       System.out.print("Lina");
+   }
+}
+
